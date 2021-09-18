6a761fd10332d30988feeefba34ef976efbd5e56
Nguyễn Văn Đức <88384447+ducnguyen995@users.noreply.github.com>
2021-09-18 20:59:39 +0700

    up

--- models/dbConnect.js
+++ models/dbConnect.js
@@ -1,17 +1,6 @@
 const mongoose = require("mongoose");
 
-<<<<<<< HEAD
-mongoose.connect("mongodb+srv://ducnguyen:anhduc1230@cluster0.ts7cd.mongodb.net/TravelBlogs?retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-});
-=======
-mongoose.connect(
-  "mongodb+srv://thaidd7b:thaidd7b@cluster0.jqh8p.mongodb.net/K14?retryWrites=true&w=majority",
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
->>>>>>> 3c8fa2a513b303308da73215e155df7daa64a04b
+mongoose.connect("mongodb+srv://ducnguyen:anhduc1230@cluster0.ts7cd.mongodb.net/TravelBlogs?retryWrites=true&w=majority", );
+
 
 module.exports = mongoose;
